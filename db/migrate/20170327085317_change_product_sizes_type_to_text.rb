891e7a3da9e280318534ea59abbe86c5c60f208a
lumbe <just.me.sober@gmail.com>
2017-04-07 22:18:22 +0000

    fix

--- db/migrate/20170327085317_change_product_sizes_type_to_text.rb
+++ db/migrate/20170327085317_change_product_sizes_type_to_text.rb
@@ -1,5 +1,5 @@
 class ChangeProductSizesTypeToText < ActiveRecord::Migration[5.0]
   def change
-    change_column :products, :sizes, :string, array: true, default: '{}'
+    change_column :products, :sizes, "USING sizes::character varying[]", default: '{}'
   end
 end
