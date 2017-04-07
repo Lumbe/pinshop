b8df7eca58eaed7cf2bd252003c070dcbee85946
lumbe <just.me.sober@gmail.com>
2017-04-07 09:58:01 +0000

    set linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application,     'pinshop'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
-# set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Don't change these unless you know what you're doing
 set :pty,             false
