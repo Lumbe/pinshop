89fdba429db38ad5acbe92aa2c16ea9647aaeb84
lumbe <just.me.sober@gmail.com>
2017-04-13 14:09:47 +0000

    fix4

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -41,7 +41,11 @@ RailsAdmin.config do |config|
     configure :image, :jcrop
 
     edit do
-      configure :description, :ck_editor
+      configure :description, :ck_editor do
+        base_location do
+          '/ckeditor'
+        end
+      end
       configure :image do
         jcrop_options aspectRatio: 420.0/512.0
       end
