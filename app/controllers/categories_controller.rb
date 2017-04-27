46524cd508b77a3c4e8623f99c0666fe5c53c072
lumbe <just.me.sober@gmail.com>
2017-04-27 07:40:33 +0000

    show 9 products in category instead of 6

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,10 +5,10 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
     respond_to do |format|
-      format.html { @products = @category.products.includes(:category).order(created_at: :desc).per_page_kaminari(params[:page]).per(6) }
+      format.html { @products = @category.products.includes(:category).order(created_at: :desc).per_page_kaminari(params[:page]).per(9) }
       format.js do
-        @products = @category.products.includes(:category).filter(params.slice(:brand)).order(created_at: :desc).per_page_kaminari(params[:page]).per(6)
-        @products = @products.where(price: params[:min_price]..params[:max_price]).order(created_at: :desc).per_page_kaminari(params[:page]).per(6) if params[:min_price] && params[:max_price]
+        @products = @category.products.includes(:category).filter(params.slice(:brand)).order(created_at: :desc).per_page_kaminari(params[:page]).per(9)
+        @products = @products.where(price: params[:min_price]..params[:max_price]).order(created_at: :desc).per_page_kaminari(params[:page]).per(9) if params[:min_price] && params[:max_price]
       end
     end
   end
