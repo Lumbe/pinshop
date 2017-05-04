072844cdf2d78df3284e9d6390817c38bf34b7ad
lumbe <just.me.sober@gmail.com>
2017-05-04 14:46:00 +0000

    MessageMailer#feedback view fix

--- spec/mailers/previews/message_mailer_preview.rb
+++ spec/mailers/previews/message_mailer_preview.rb
@@ -1,5 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/message_mailer
 class MessageMailerPreview < ActionMailer::Preview
+  def feedback
+    MessageMailer.feedback('example@email.com', 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.')
+  end
   def new_order
     MessageMailer.new_order('example@email.com', Order.last)
   end
